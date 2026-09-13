e4dcec37ed25dc8ae3de8579351e534f849e0e3b
Luis Marrero <luismarrer.dev@gmail.com>
2026-09-13 00:39:14 -0400

    chore: support BASE_URL environment variable in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'list',
   use: {
-    baseURL: 'http://127.0.0.1:4321',
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:4321',
     trace: 'on-first-retry',
   },
   projects: [
